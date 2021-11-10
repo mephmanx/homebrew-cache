6cc680fbfd302337f23d63e9e2518ed9fe13ab47
Chris Lyons <chris@lyonsgroup.family>
2021-11-09 21:14:00 -0500

    Initial version

--- init.sh
+++ init.sh
@@ -13,3 +13,5 @@ tfenv install $1
 tfenv use $1
 mkdir -p /tmp/export
 tar -zcvf /tmp/export/homebrew.tar.gz /home/linuxbrew
+gunzip /tmp/export/homebrew.tar.gz
+mv /tmp/export/homebrew.tar /tmp/export/homebrew-$1.tar
