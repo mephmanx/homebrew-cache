70b4cb840e8afd9df3f48bcfb4b213f19f5fa9b3
Chris Lyons <chris@lyonsgroup.family>
2021-12-01 07:42:09 -0500

    Initial version

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Building cache for version -> $1"
+echo "Building homebrew cache and configuring active terraform version as -> $1"
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh -o /tmp/homebrew.sh > /dev/null
 cd /tmp
 chmod +x /tmp/homebrew.sh
