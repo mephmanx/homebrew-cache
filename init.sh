14d3bfabc6bc1f7070e33f0837dc03b72a64da7e
Chris Lyons <chris@lyonsgroup.family>
2021-12-01 17:26:31 -0500

    Initial version

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building homebrew cache and configuring active terraform version as -> $1"
-exit
+
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh -o /tmp/homebrew.sh > /dev/null
 cd /tmp
 chmod +x /tmp/homebrew.sh
@@ -12,10 +12,10 @@ brew install cloudfoundry/tap/bosh-cli
 brew install bbl
 brew unlink terraform
 brew install tfenv
-tfenv install $@
-tfenv use $@
+tfenv install $1
+tfenv use $1
 mkdir -p /tmp/export
 cd /home/linuxbrew
 tar -zcvf /tmp/export/homebrew.tar.gz .linuxbrew
 gunzip /tmp/export/homebrew.tar.gz
-mv /tmp/export/homebrew.tar /tmp/export/homebrew-$@.tar
+mv /tmp/export/homebrew.tar /tmp/export/homebrew-$1.tar
