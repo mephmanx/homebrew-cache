d3468bbcd0f36e13d551c1ee87278f50c7c53c8c
chris <chris@lyonsgroup.family>
2022-08-04 18:24:24 -0400

    cleanup

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Building homebrew cache and configuring active terraform version as -> $1"
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 brew install cloudfoundry/tap/bosh-cli
 brew install bbl
 brew install cloudfoundry/tap/cf-cli@8
 brew unlink terraform
 brew install tfenv
-tfenv install $1
-tfenv use $1
+tfenv install "$1"
+tfenv use "$1"
 mkdir -p /tmp/export
-cd /home/linuxbrew
+cd /home/linuxbrew || exit
 tar -zcvf /tmp/export/homebrew.tar.gz .linuxbrew
 gunzip /tmp/export/homebrew.tar.gz
-mv /tmp/export/homebrew.tar /tmp/export/homebrew-$1.tar
+mv /tmp/export/homebrew.tar /tmp/export/homebrew-"$1".tar
