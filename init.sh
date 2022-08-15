2bb1e7fb4b5b0d74f09590879b4112d2a9ecbe9a
chris <chris@lyonsgroup.family>
2022-08-15 10:59:59 -0400

    upstream was merged, reverting to original repo

--- init.sh
+++ init.sh
@@ -3,17 +3,9 @@
 echo "Building homebrew cache and configuring active terraform version as -> $1"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
-
-#temp to use this repo, after upstream get merged, just delete those lines.
-brew tap yaguangtang/tap
-brew install yaguangtang/tap/bosh-cli
-brew install yaguangtang/tap/bbl
-brew install yaguangtang/tap/cf-cli@8
-
-#brew install cloudfoundry/tap/bosh-cli
-#brew install bbl
-#brew install cloudfoundry/tap/cf-cli@8
-
+brew install cloudfoundry/tap/bosh-cli
+brew install bbl
+brew install cloudfoundry/tap/cf-cli@8
 brew unlink terraform
 brew install tfenv
 tfenv install "$1"
